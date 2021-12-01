8ccb46099d4b888a89cd20ff3226dc1daf41bafa
Kei Hibino <ex8k.hibino@gmail.com>
2021-12-01 15:54:39 +0900

    chap12 code: update example code of 12.3.

--- Chap12.3.hs
+++ Chap12.3.hs
@@ -11,9 +11,6 @@ type Line = [Word]
 
 -- para =
 
-maxWidth :: Nat
-maxWidth = 80
-
 fits :: Line -> Bool
 fits line = width line <= maxWidth
 
@@ -38,9 +35,6 @@ cost3 :: [Line] -> Nat
 cost3 = sum . map waste . init
   where waste line = (optWidth - width line)^(2 :: Int)
 
-optWidth :: Nat
-optWidth = 72
-
 cost4 :: [Line] -> Nat
 cost4 = foldr max 0 . map waste . init
   where waste line = maxWidth - width line
@@ -54,5 +48,14 @@ greedy = foldl add []
   where add [] w = snoc w []
         add p  w = head (filter (fits . last) [bind w p, snoc w p])
 
+maxWidth :: Nat
+maxWidth = 10
+
+optWidth :: Nat
+optWidth = 8
+
 example :: String
-example = "The algorithm works by adding each word to the end of the last line of the current  paragraph until no more words will fit, in which case a new line is started. A more  efficient version is discussed in the exercises. This algorithm is essentially the one  used by Microsoft Word and many other word processors.  So, for which definition of cost does the greedy algorithm work?"
+example = "666666 1 55555 333 4444 7777777"
+
+exampleL :: String
+exampleL = "The algorithm works by adding each word to the end of the last line of the current  paragraph until no more words will fit, in which case a new line is started. A more  efficient version is discussed in the exercises. This algorithm is essentially the one  used by Microsoft Word and many other word processors.  So, for which definition of cost does the greedy algorithm work?"
