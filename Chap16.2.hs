cf90dbc124f075ea2ba4e7fe269cc6d5242f8e7f
Kei Hibino <ex8k.hibino@gmail.com>
2022-11-06 19:56:12 +0900

    chap16: hs: 16.2: add example graph

--- Chap16.2.hs
+++ Chap16.2.hs
@@ -5,8 +5,12 @@ import qualified Data.Set as S
 
 ---
 
+data ExampleVs
+  = A | B | C | D | E | F
+  deriving (Eq, Ord, Show)
+
 type Nat = Int
-type Vertex = ()
+type Vertex = ExampleVs
 
 ---
 
@@ -101,3 +105,37 @@ mstar g h goal source = msearch S.empty start
 succs :: Graph -> Heuristic -> S.Set Vertex -> Path -> [(Path,Cost)]
 succs g h vs p = [extend p v d | (v,d) <- g (end p),not (S.member v vs)]
   where extend (vs, c) v d = ((v:vs, c+d), c+d + h v)
+
+---
+
+h161 :: Heuristic
+h161 v = case v of
+  A -> 9
+  B -> 1
+  C -> 5
+  D -> 0
+
+g161 :: Graph
+g161 A = [(B, 5), (C, 2)]
+g161 B = [(D, 5)]
+g161 C = [(B, 2)]
+g161 D = []
+
+---
+
+h :: Heuristic
+h v = case v of
+  A -> 10
+  B -> 10
+  C -> 5
+  D -> 5
+  E -> 0
+  F -> 0
+
+g :: Graph
+g A = [(B, 3), (C, 10), (D, 20), (E, 20)]
+g B = [(A, 3), (C, 5), (D, 8), (E, 20)]
+g C = [(A, 10), (B, 5), (D, 2), (E, 10)]
+g D = [(A, 20), (B, 8), (C, 2), (E, 6)]
+g E = [(A, 20), (B, 20), (C, 10), (D, 6), (F, 1)]
+g F = []
